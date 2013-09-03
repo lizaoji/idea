76f68907151799ff14efda853f5f4b48ab161c06
Ben Lee <lzlzj0407@gmail.com>
2013-09-03 18:33:51 +0800

    My first app

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.text = params[:post][:text]
+    if @post.save
+      redirect_to :root
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Idea::Application.routes.draw do
+  root 'home#index'
+  get "home/index"
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
